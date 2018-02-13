de4222a9b7991068243b48d2714850a1765b2d3c
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2018-02-13 11:54:39 +0300

    contract project created

---
+++ IcoShareSC/IcoShareContract.cs
@@ -0,0 +1,15 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace IcoShareSC
+{
+    public class IcoShareContract : SmartContract
+    {
+        public static void Main()
+        {
+            Storage.Put(Storage.CurrentContext, "Hello", "World");
+        }
+    }
+}
